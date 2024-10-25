b9b404b957fa06426913e7d8755f5ac3e9537479
Kedar Khaire <kedark@google.com>
2024-10-25 11:35:58 +0530

    Update KeyValueMapDenormalizer.php for denormalize

--- src/Denormalizer/KeyValueMapDenormalizer.php
+++ src/Denormalizer/KeyValueMapDenormalizer.php
@@ -29,7 +29,7 @@ class KeyValueMapDenormalizer implements DenormalizerInterface
     /**
      * {@inheritdoc}
      */
-    public function denormalize($data, $type, $format = null, array $context = [])
+    public function denormalize($data, $type, $format = null, array $context = []): mixed
     {
         return new $type($data);
     }
